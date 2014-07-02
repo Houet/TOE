8a023dfd387e1e8de5d066351c04c277f63e5908
Corentin <corentin.houet@gmail.com>
2014-07-02 12:20:01 +0200

    maj logging

--- toe.py
+++ toe.py
@@ -211,7 +211,12 @@ def compare(time1,time2):
 
 if __name__ == '__main__' :
 
-	logging.basicConfig(level=logging.WARNING)
+	loglevel=str(sys.argv[1])
+	numeric_level = getattr(logging, loglevel.upper(), None)
+	if not isinstance(numeric_level, int) :
+		raise ValueError('Invalid log level: %s' % loglevel)
+	
+	logging.basicConfig(level=numeric_level)
 
 	try :
 		CONSUMER_KEY=get_env_var('CONSUMER_KEY')
