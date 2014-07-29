4c595f34038cf60c0ccc1e3e0775a2757e1cc78a
Corentin <corentin.houet@gmail.com>
2014-07-29 11:37:18 +0200

    patch 3.0 move to py3.4

--- toe.py
+++ toe.py
@@ -4,13 +4,12 @@
 
 import urllib
 import json
-import twitter
 import os
 import sys
 import logging
-import pytz
 import time
-import datetime
+import pytz
+from twitter import Twitter, OAuth, TwitterError
 from pytz import timezone
 from datetime import datetime, timedelta
 import locale
@@ -26,6 +25,22 @@ URL_FILTER = "http://renass.unistra.fr/evenements"
 class MissingValue(Exception):
     """ exception raises when a value is missing  """
     pass
+
+
+class TweetEvent():
+    """ """
+    def __init__(self, geojson):
+        """ """
+        self.description = geojson["properties"]["description"]
+        self.url = geojson["properties"]["url"]
+        self.date = conversion(geojson["properties"]["time"])
+
+        self.lat = geojson["geometry"]["coordinates"][1]
+        self.lon = geojson["geometry"]["coordinates"][0]
+
+    def unicode(self):
+        """ """
+        return
 
 
 def get_env_var(varname, default=None):
@@ -44,16 +59,15 @@ def conversion(string):
     naive = pytz.utc.localize(utc_dt)
     local_dt = naive.astimezone(LOCAL)
     string = local_dt.strftime('le %d %B %Y à %H:%M:%S heure locale')
-    string = string.decode('utf-8')
     return string
 
 
-def get_twitter_timeline(api):
+def get_twitter_timeline():
     """ Get the twitter timeline """
 
     try:
-        timeline = api.GetHomeTimeline(count=150)
-    except twitter.TwitterError, exception:
+        timeline = api.statuses.home_timeline(count=150)
+    except TwitterError as exception:
         logging.error(exception)
         sys.exit(2)
 
@@ -63,21 +77,22 @@ def get_twitter_timeline(api):
 def read_json(url):
     """ recover data from webservice in format json """
     logging.info(url)
-    sock = urllib.urlopen(url)
     try:
-        data_json = json.load(sock)
-        sock.close()
-    except ValueError:
+        sock = urllib.request.urlopen(url)
+        sock = sock.read()
+    except urllib.error.HTTPError:
         logging.warning("decoding Json has failed")
         sys.exit(3)
+    else:
+        data_json = json.loads(bytes.decode(sock))
+        return data_json
 
-    return data_json
 
-
-def get_startime_from_twitter(api):
+def get_startime_from_twitter():
     """ get the date of the last earthquake published """
-    twitter_timeline = get_twitter_timeline(api)
-    list_url = [t.urls[0].expanded_url for t in twitter_timeline if t.urls]
+    twitter_timeline = get_twitter_timeline()
+    list_url = [t['entities']['urls'][0]['expanded_url'] for
+                t in twitter_timeline if t['entities']['urls']]
     filtered_list = [u for u in list_url if URL_FILTER in u]
     logging.debug('list of event: %s', filtered_list)
     try:
@@ -90,7 +105,7 @@ def get_startime_from_twitter(api):
         date_post = time.gmtime(twitter_timeline[0].created_at_in_seconds)
         return time.strftime('%Y-%m-%dT%H:%M:%S', date_post)
     else:
-        url_id = URL_SEARCH + urllib.urlencode(url_argv.items())
+        url_id = URL_SEARCH + urllib.parse.urlencode(url_argv)
         logging.info('url of the last earthquake published :')
         startime_twit = read_json(url_id)['features'][0]['properties']['time']
         return startime_twit
@@ -104,20 +119,21 @@ def get_starttime_from_yesterday():
     return yesterday
 
 
-def get_most_recent_starttime(api):
+def get_most_recent_starttime():
     """get the most recent startime """
     date_yesterday = get_starttime_from_yesterday()
     logging.info('start time from user: %s', date_yesterday)
 
-    date_twitter = datetime.strptime(get_startime_from_twitter(api), '%Y-%m-%dT%H:%M:%S') + timedelta(0, 1)
+    date_twitter = datetime.strptime(get_startime_from_twitter(),
+                                     '%Y-%m-%dT%H:%M:%S') + timedelta(0, 1)
     logging.info('start time from twitter: %s', date_twitter)
 
     return max(date_twitter, date_yesterday)
 
 
-def get_data_to_publish(api):
+def get_data_to_publish():
     """ get data to publish """
-    starttime = get_most_recent_starttime(api).strftime('%Y-%m-%dT%H:%M:%S')
+    starttime = get_most_recent_starttime().strftime('%Y-%m-%dT%H:%M:%S')
     logging.info("most recent start time : %s", starttime)
     minmagnitude = get_env_var("MAGNITUDE_MIN", 2)
 
@@ -132,17 +148,17 @@ def get_data_to_publish(api):
         "minmagnitude": minmagnitude,
     }
 
-    webservice = URL_SEARCH + urllib.urlencode(url_arg.items())
+    webservice = URL_SEARCH + urllib.parse.urlencode(url_arg)
     logging.info("url of data we recovered")
     data_recovered = read_json(webservice)
 
     return data_recovered
 
 
-def formatting(api):
+def formatting():
     """ preformating tweet in order to publish them """
 
-    data = get_data_to_publish(api)
+    data = get_data_to_publish()
 
     url = [u['properties']['url'] for u in data['features']]
     date = [t['properties']['time'] for t in data['features']]
@@ -152,7 +168,7 @@ def formatting(api):
     lat = [l['geometry']['coordinates'][1] for l in data['features']]
     lon = [l['geometry']['coordinates'][0] for l in data['features']]
 
-    message = zip(description, date, url, lat, lon)
+    message = list(zip(description, date, url, lat, lon))
     message.reverse()
     logging.info("data to publish: %s", message)
 
@@ -164,8 +180,9 @@ def publish(tweet, latitude=None, longitude=None):
     """ publish data on twitter"""
 
     try:
-        api.PostUpdate(tweet, latitude=latitude, longitude=longitude)
-    except twitter.TwitterError, exception:
+        api.statuses.update(status=tweet, lat=latitude,
+                            long=longitude)
+    except TwitterError as exception:
         logging.error(exception)
         sys.exit(2)
 
@@ -187,15 +204,12 @@ if __name__ == '__main__':
 
     # authentification
     try:
-        key_dict = {
-            'consumer_key': get_env_var('CONSUMER_KEY'),
-            'consumer_secret': get_env_var('CONSUMER_SECRET'),
-            'access_token_key': get_env_var('ACCES_TOKEN_KEY'),
-            'access_token_secret': get_env_var('ACCES_TOKEN_SECRET'),
-        }
-    except MissingValue, exception:
+        api = Twitter(auth=OAuth(get_env_var('ACCES_TOKEN_KEY'),
+                                 get_env_var('ACCES_TOKEN_SECRET'),
+                                 get_env_var('CONSUMER_KEY'),
+                                 get_env_var('CONSUMER_SECRET')))
+    except MissingValue as exception:
         logging.error(exception)
         sys.exit(1)
 
-    api = twitter.Api(**key_dict)
-    formatting(api)
+    formatting()
